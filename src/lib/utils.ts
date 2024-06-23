2cab682820802dc5a47a5fa02c2011379e4abc9f
harshmangalam <harshdev8218@gmail.com>
2024-06-23 07:45:36 +0530

    added location utility fn

---
+++ src/lib/utils.ts
@@ -0,0 +1,12 @@
+async function reverseGeocodingAPI(lat: number, lon: number) {
+  const resp = await fetch(
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=${Bun.env.GEOAPIFY_API_KEY}`
+  );
+  if (resp.ok) {
+    const jsonResp = await resp.json();
+    const data = jsonResp?.features[0]?.properties;
+    return data;
+  }
+}
+
+export { reverseGeocodingAPI };
