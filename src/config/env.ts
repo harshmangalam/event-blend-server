54abe3e2698a2f51e6e48cec71680cba11e2ac6d
harshmangalam <harshdev8218@gmail.com>
2024-06-24 11:59:14 +0530

    extend Bun env for typesafty

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,26 +1,31 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  PORT: z.coerce.number().min(1000).max(65535),
+  PORT: z.coerce.number().min(1000).max(65535).optional().default(3000),
   ENV: z
     .union([
       z.literal("development"),
       z.literal("testing"),
       z.literal("production"),
     ])
+    .optional()
     .default("development"),
-  DATABASE_URL: z.string(),
-  GEOAPIFY_API_KEY: z.string(),
-  JWT_ACEESS_TOKEN_SECRET: z.string(),
-  JWT_REFRESH_TOKEN_SECRET: z.string(),
+  DATABASE_URL: z.string().url(),
+  GEOAPIFY_API_KEY: z.string().trim().min(1),
+  JWT_ACEESS_TOKEN_SECRET: z.string().trim().min(1),
+  JWT_REFRESH_TOKEN_SECRET: z.string().trim().min(1),
 });
 
 declare module "bun" {
   interface Env extends z.infer<typeof envSchema> {}
 }
+const parsedEnv = envSchema.safeParse(Bun.env);
 
-const env = envSchema.parse(Bun.env);
+if (!parsedEnv.success) {
+  console.error(parsedEnv.error.issues);
+  process.exit(1);
+}
+export const env = parsedEnv.data;
 export type Environment = {
   Bindings: z.infer<typeof envSchema>;
 };
-export default env;
