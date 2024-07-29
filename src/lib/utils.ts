f2345c25865ddea662455a6942d0e37a319a61d0
harshmangalam <harshdev8218@gmail.com>
2024-07-29 13:52:47 +0530

    generate slug from str

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -36,9 +36,19 @@ function paginate(page?: number, pageSize?: number) {
   const take = pageSize;
   return [take, skip];
 }
+
+function generateSlug(string: string) {
+  return string
+    .toLowerCase() // Convert to lowercase
+    .trim() // Remove whitespace from both sides
+    .replace(/[^\w\s-]/g, "") // Remove all non-word characters and non-whitespace characters except for hyphens
+    .replace(/\s+/g, "-") // Replace spaces with hyphens
+    .replace(/-+/g, "-"); // Replace multiple hyphens with a single hyphen
+}
 export {
   reverseGeocodingAPI,
   getExpTimestamp,
   extractDuplicatePrismaField,
   paginate,
+  generateSlug,
 };
