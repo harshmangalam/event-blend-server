91841992cd7ed98742df31235709717ccd0ec5e6
harshmangalam <harshdev8218@gmail.com>
2024-10-28 21:48:17 +0530

    added seeding

--- src/feature/seed/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export const usersSeedSchema = z.object({
-  role: z.enum(["User", "Admin"]),
-  num: z.coerce.number().optional(),
-});
-
-export const seedSchema = z.object({
-  num: z.coerce.number().optional(),
-});
